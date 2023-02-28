dbdb8157bd6fcb941124e64ba99cfbcfbef7c24b
akhilayyanki <akhilayyanki@gmail.com>
2023-02-28 18:11:47 -0500

    Parsing requests & sending responses

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const http = require('http'); //'./http'->searches for a local file
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method,req.headers);
+
+    const url = req.url;
+    const method = req.method;
+    if(url==='/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>A node course</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('<html>');
+        return res.end();
+    }
+
+    if(url==='/message'&&method=='POST'){
+        const body = []
+        req.on('data',(chunk)=> {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt',message);
+        });
+        //redirecting to another page
+        res.statusCode =302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
+
+
 }); //each request upon receipt would execute requestListener
 // this is event driven architecture - If X happens do Y
 
