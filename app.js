8ea0b016f2d33bbe16da759a5ce82b83f2046fd7
akhilayyanki <akhilayyanki@gmail.com>
2023-03-04 13:15:49 -0500

    express.js basic setup

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-const http = require('http'); //'./http'->searches for a local file
-const routes = require('./routes');
+const http = require('http'); 
 
-const server = http.createServer(routes.handler); 
-//each request upon receipt would execute requestListener
-// this is event driven architecture - If X happens do Y
+const express = require('express');
+
+const app = express();
+
+//executed for every request
+// express uses the 'startswith' operator for path
+
+app.use('/',(req,res,next)=>{
+    console.log('zero middlewaar');
+    next();
+
+});
+
+app.use('/add-product',(req,res,next)=>{
+    console.log('first middlewaar');
+    res.send('<h1>Its the product page!</h1>');
+});
+
+// a next should be directed at the same route but different need.
+app.use('/',(req,res,next)=>{
+    console.log('second middlewaar');
+    res.send('<h1>Using using express.js!!</h1>');
+});
+
+
+
+
+const server = http.createServer(app); 
 
 server.listen(3000);
