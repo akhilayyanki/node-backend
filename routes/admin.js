64d8bdcb5c0b7048dcdc8c3ae8f61ecfb9f987be
akhilayyanki <akhilayyanki@gmail.com>
2023-03-06 19:10:02 -0500

    prefix routes in express Router

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+//changes to GET "admin/add-product" , admin already checked earlier.
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="product-title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="product-title"><button type="submit">Add Product</button></form>');
 });
 
 // a next should be directed at the same route but different need.
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
     console.log(req.body);//added by express
     res.redirect('/');
 });
